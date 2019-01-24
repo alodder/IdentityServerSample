1afb6315935d2e9f240efffe1b77edc2d11f07ae
LODDER Adrian <loddera@ccb.state.or.us>
2019-01-24 11:38:41 -0800

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            DoStuff().Wait();
+
+            Console.ReadKey();
+        }
+
+        public static async Task DoStuff()
+        {
+            TokenResponse tokenResponse = await GetToken();
+            GetAPIValues( tokenResponse).Wait();
+        }
+
+        public static async Task<TokenResponse> GetToken()
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return null;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return null;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            return tokenResponse;
+        }
+
+        private static async Task GetAPIValues(TokenResponse tokenResponse)
+        {
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
